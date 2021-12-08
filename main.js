27b3d77efdfb94df7fdfb4eb4e5136080e877285
awesomefanfilly1453 <89775996+awesomefanfilly1453@users.noreply.github.com>
2021-12-08 20:51:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("project on console");
+var firstname="Elaine";
+var secondname="Rose";
+var thirdname="Rodrigues";
+var fullname=firstname.concat(secondname);
+var finalname=fullname.concat(thirdname);
+console.log(finalname);
+var a=2;
+var b=9;
+var add=a+b;
+console.log(a,b,add);
